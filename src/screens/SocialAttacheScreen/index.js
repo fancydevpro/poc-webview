b9e3a8f54d2314a38c9ef000c1b48582c77ab50d
hamza1216 <hamza.el.kedmiri.1216@gmail.com>
2021-06-24 09:25:06 -0700

    Implemented wevview navigation

--- src/screens/SocialAttacheScreen/index.js
+++ src/screens/SocialAttacheScreen/index.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import {
   Text,
   View,
   StyleSheet,
   SafeAreaView,
   Alert,
+  Dimensions,
   Platform,
+  ActivityIndicator,
   TouchableOpacity,
 } from 'react-native';
 import { WithLocalSvg } from 'react-native-svg';
@@ -17,44 +19,163 @@ const SocialAttacheIcon = require('../../assets/images/CompanySocialAttacheClr.s
 const FacebookIcon = require('../../assets/images/CompanyFacebookDk.svg');
 const InstagramIcon = require('../../assets/images/CompanyInstagramDk.svg');
 const LinkedInIcon = require('../../assets/images/CompanyLinkedInDk.svg');
+const PinterestIcon = require('../../assets/images/CompanyPinterestDk.svg');
+const TikTokIcon = require('../../assets/images/CompanyTikTokDk.svg');
+const TwitterIcon = require('../../assets/images/CompanyTwitterDk.svg');
 
 const SocialAttachUrl = 'https://socialattache.com/';
 const FacebookUrl = 'https://www.facebook.com/';
+const InstagramUrl = 'https://www.instagram.com/';
+const LinkedInUrl = 'https://www.linkedin.com/';
+const PinterestUrl = 'https://www.pinterest.com/';
 const TiktokUrl = 'https://www.tiktok.com/';
+const Twitterurl = 'https://www.twitter.com/';
+
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get('screen');
 
 const SocialAttacheScreen = ({ navigation, route }) => {
-  const webView = useRef();
-  const cookieRef = useRef({value: ''});
+  const [canGoBackMain, setCanGoBackMain] = useState(false);
+  const [canGoForwardMain, setCanGoForwardMain] = useState(false);
+  const [canGoBackSocial, setCanGoBackSocial] = useState(false);
+  const [canGoForwardSocial, setCanGoForwardSocial] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [webviewMode, setWebviewMode] = useState(0); // 0: SocialAttache, 1: Social site
+
+  const mainWebView = useRef(null);
+  const socialWebView = useRef(null);
+
+  const mainCookieRef = useRef({value: ''});
+  const socialCookieRef = useRef({value: ''});
+  
   //const weburl = 'http://192.168.1.30/webview.php'
   //const weburl = 'https://tiktok.com/'
   //const weburl = 'https://www.facebook.com/'  
 
-  const [weburl, setWebUrl] = useState(SocialAttachUrl)
+  const [mainWebviewUrl, setMainWebviewUrl] = useState(SocialAttachUrl);
+  const [socialWebviewurl, setSocialWebviewurl] = useState(FacebookUrl);
 
-  // handle message from webpage
-  const onBridgeMessage = (event) => {
+  // social attache
+  const onPressedSocialAttache = () => {
+    setWebviewMode(0);
+    setMainWebviewUrl(SocialAttachUrl);
+  };
+  // facebook
+  const onPressedFacebook = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(FacebookUrl);
+  };
+  // instagram
+  const onPressedInstagram = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(InstagramUrl);
+  }
+  // linkedin
+  const onPressedLinkedIn = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(LinkedInUrl);
+  }
+  // pinterest
+  const onPressedPinterest = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(PinterestUrl);
+  }
+  // tiktok
+  const onPressedTiktok = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(TiktokUrl);
+  }
+  // twitter
+  const onPressedTwitter = () => {
+    setWebviewMode(1);
+    setSocialWebviewurl(Twitterurl);
+  }
+  // forwards
+  const onBackwardPressed = useCallback(() => {
+    if(webviewMode == 0) {
+      if(canGoBackMain) {
+        console.log('main can go back')
+        mainWebView.current.goBack();
+      } else{        
+        console.log('main can not go back');        
+      }
+    } else {
+      if(canGoBackSocial) {
+        socialWebView.current.goBack();
+      } 
+    }
+  }, [webviewMode, mainWebView, socialWebView, canGoBackMain, canGoBackSocial]);
+  // backwards
+  const onForwardPressed = useCallback(() => {
+    if(webviewMode == 0) {
+      if(canGoForwardMain) {
+        mainWebView.current.goForward();
+      }
+    } else {
+      if(canGoForwardSocial) {
+        socialWebView.current.goForward();
+      }
+    }
+  }, [webviewMode, mainWebView, socialWebView, canGoForwardMain, canGoForwardSocial]);
+  // main navigation state changed
+  const onMainNavigationStateChange = (navState) => {
+    setCanGoBackMain(navState.canGoBack);
+    setCanGoForwardMain(navState.canGoForward);
+    console.log('Main NavState', navState.url, navState.canGoBack, navState.canGoForward);
+  }
+  // social navigation state changed
+  const onSocialNavigationStateChange = (navState) => {
+    setCanGoBackSocial(navState.canGoBack);
+    setCanGoForwardSocial(navState.canGoForward);
+    console.log('Social NavState', navState.url, navState.canGoBack, navState.canGoForward);
+  }
+
+  // handle message from main webpage
+  const onMainWebviewBridgeMessage = (event) => {
     const { data } = event.nativeEvent;    
     const jsonData = JSON.parse(data);
     console.log(jsonData);    
     if(jsonData.type == 'message') {
       Alert.alert('PocWeb', jsonData.value);
     } else if(jsonData.type == 'cookie'){   
-      cookieRef.current.value = jsonData.value;
+      mainCookieRef.current.value = jsonData.value;
       //Alert.alert('Cookie from the webview', jsonData.value);
     }
   }
 
-  // get cookie 
-  const getCookie = () => {
+  // handle message from main webpage
+  const onSocialWebviewBridgeMessage = (event) => {
+    const { data } = event.nativeEvent;    
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);    
+    if(jsonData.type == 'message') {
+      Alert.alert('PocWeb', jsonData.value);
+    } else if(jsonData.type == 'cookie'){   
+      socialCookieRef.current.value = jsonData.value;
+      //Alert.alert('Cookie from the webview', jsonData.value);
+    }
+  }
+
+  // get cookie for main webview
+  const getMainWebviewCookie = () => {
     const javascriptCodeToSend = `
       window.ReactNativeWebView.postMessage(JSON.stringify({type: 'cookie', value: document.cookie}));
       true;
     `;
-    webView.current.injectJavaScript(javascriptCodeToSend);
+    mainWebView.current.injectJavaScript(javascriptCodeToSend);
+  }
+
+  // get cookie for social webview
+  const getSocialWebviewCookie = () => {
+    const javascriptCodeToSend = `
+      window.ReactNativeWebView.postMessage(JSON.stringify({type: 'cookie', value: document.cookie}));
+      true;
+    `;
+    socialWebView.current.injectJavaScript(javascriptCodeToSend);
   }
   // button handler to show cookie
   const onPressedShowCookie = () => {
-    getCookie();
+    getMainWebviewCookie();
   }
 
   const requestAjaxCallWithCookie = async (cookie) => {
@@ -77,19 +198,6 @@ const SocialAttacheScreen = ({ navigation, route }) => {
     `;
     webView.current.injectJavaScript(javascriptCodeToSend);
   }
-
-  const onPressedSocialAttache = () => {
-    setWebUrl(SocialAttachUrl);
-  }
-
-  const onPressedFacebook = () => {
-    setWebUrl(FacebookUrl);
-  }
-
-  const onNavigationStateChange = (navState) => {
-    console.log('NavState', navState.url);
-  }
-
   return (
       <SafeAreaView style={styles.container}>
         <View
@@ -97,14 +205,16 @@ const SocialAttacheScreen = ({ navigation, route }) => {
           <View
             style={styles.headerLeftView}>
             <TouchableOpacity
-              style={styles.headerButton}>
+              style={styles.headerButton}
+              onPress={onBackwardPressed}>
               <WithLocalSvg
                 width={24}
                 height={24}
                 asset={BackArrow}/>
             </TouchableOpacity>
             <TouchableOpacity
-              style={styles.headerButton}>
+              style={styles.headerButton}
+              onPress={onForwardPressed}>
               <WithLocalSvg
                 width={24}
                 height={24}
@@ -130,39 +240,101 @@ const SocialAttacheScreen = ({ navigation, route }) => {
                 asset={FacebookIcon}/>
             </TouchableOpacity>
             <TouchableOpacity
-              style={styles.headerButton}>
+              style={styles.headerButton}
+              onPress={onPressedInstagram}>
               <WithLocalSvg
                 width={24}
                 height={24}
                 asset={InstagramIcon}/>
             </TouchableOpacity>
             <TouchableOpacity
-              style={styles.headerButton}>
+              style={styles.headerButton}
+              onPress={onPressedLinkedIn}>
               <WithLocalSvg
                 width={24}
                 height={24}
                 asset={LinkedInIcon}/>
             </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.headerButton}
+              onPress={onPressedPinterest}>
+              <WithLocalSvg
+                width={24}
+                height={24}
+                asset={PinterestIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.headerButton}
+              onPress={onPressedTiktok}>
+              <WithLocalSvg
+                width={24}
+                height={24}
+                asset={TikTokIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.headerButton}
+              onPress={onPressedTwitter}>
+              <WithLocalSvg
+                width={24}
+                height={24}
+                asset={TwitterIcon}/>
+            </TouchableOpacity>
           </View>
         </View>
-        <WebView
-          ref={webView}
-          onLoad={() => getCookie()}
-          source={{
-            uri: weburl,
-            // headers: {
-            //   Cookie: 'user=john; password=123456',
-            // },
-          }}            
-          userAgent={Platform.OS == 'ios' ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1' :
-          'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36'}
-          allowsInlineMediaPlayback="true"
-          onMessage={onBridgeMessage}
-          javaScriptEnabled={true}
-          sharedCookiesEnabled={true} 
-          thirdPartyCookiesEnabled={true}
-          onNavigationStateChange={onNavigationStateChange}
-        />
+        {loading && <ActivityIndicator
+            style={{
+                flex: 1,
+                width: '10%',
+                height: '10%',
+                position: 'absolute',
+                top: SCREEN_HEIGHT / 2 - 100,
+                zIndex: 1,
+                alignSelf: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+            animating={loading}
+            size="large"            
+        />}
+        {
+          webviewMode === 0 ?
+          <WebView
+            ref={mainWebView}
+            onLoad={() => getMainWebviewCookie()}
+            onLoadEnd={() => setLoading(false)}
+            onLoadStart={() => setLoading(true)}
+            source={{
+              uri: mainWebviewUrl,
+            }}
+            allowsInlineMediaPlayback="true"
+            onMessage={onMainWebviewBridgeMessage}
+            javaScriptEnabled={true}
+            sharedCookiesEnabled={true} 
+            thirdPartyCookiesEnabled={true}
+            onNavigationStateChange={onMainNavigationStateChange}
+          /> :
+          <WebView
+            ref={socialWebView}
+            onLoad={() => getSocialWebviewCookie()}
+            onLoadEnd={() => setLoading(false)}
+            onLoadStart={() => setLoading(true)}
+            source={{
+              uri: socialWebviewurl,
+              // headers: {
+              //   Cookie: 'user=john; password=123456',
+              // },
+            }}
+            // userAgent={Platform.OS == 'ios' ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1' :
+            // 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36'}
+            allowsInlineMediaPlayback="true"
+            onMessage={onSocialWebviewBridgeMessage}
+            javaScriptEnabled={true}
+            sharedCookiesEnabled={true} 
+            thirdPartyCookiesEnabled={true}
+            onNavigationStateChange={onSocialNavigationStateChange}
+          />
+        }
+
     </SafeAreaView>
   );
 }
